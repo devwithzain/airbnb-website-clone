f3978b3a6aa00d1dce01b55e72ac2b448c8a2666
Muhammad Zain <ZainSoftwear11@gmail.com>
2024-03-31 15:18:54 +0500

    completed

---
+++ app/favorites/FavoritesClient.tsx
@@ -0,0 +1,40 @@
+import {
+	Heading,
+	Container,
+	ListingCard,
+	TFavoritesClientProps,
+} from "@/export";
+
+export default function FavoritesClient({
+	listings,
+	currentUser,
+}: TFavoritesClientProps) {
+	return (
+		<Container>
+			<Heading
+				title="Favorites"
+				subtitle="List of places you favorited!"
+			/>
+			<div
+				className="
+          mt-10
+          grid 
+          grid-cols-1 
+          sm:grid-cols-2 
+          md:grid-cols-3 
+          lg:grid-cols-4
+          xl:grid-cols-5
+          2xl:grid-cols-6
+          gap-8
+        ">
+				{listings.map((listing: any) => (
+					<ListingCard
+						currentUser={currentUser}
+						key={listing.id}
+						data={listing}
+					/>
+				))}
+			</div>
+		</Container>
+	);
+}
---
+++ app/favorites/page.tsx
@@ -0,0 +1,32 @@
+import {
+	EmptyState,
+	ClientOnly,
+	getCurrentUser,
+	getFavoriteListings,
+} from "@/export";
+import FavoritesClient from "./FavoritesClient";
+
+export default async function ListingPage() {
+	const listings = await getFavoriteListings();
+	const currentUser = await getCurrentUser();
+
+	if (listings.length === 0) {
+		return (
+			<ClientOnly>
+				<EmptyState
+					title="No favorites found"
+					subtitle="Looks like you have no favorite listings."
+				/>
+			</ClientOnly>
+		);
+	}
+
+	return (
+		<ClientOnly>
+			<FavoritesClient
+				listings={listings}
+				currentUser={currentUser}
+			/>
+		</ClientOnly>
+	);
+}
