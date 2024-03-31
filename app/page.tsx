41c01da2c5d2431bfebd4aaeed5d9ced019a94fe
Muhammad Zain <ZainSoftwear11@gmail.com>
2024-03-31 15:15:26 +0500

    modified

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,48 @@
-export default function App() {
-	return <div>App</div>;
+import {
+	getCurrentUser,
+	ListingCard,
+	EmptyState,
+	ClientOnly,
+	getListings,
+	Container,
+	THomeProps,
+} from "@/export";
+
+export default async function App({ searchParams }: THomeProps) {
+	const listings = await getListings(searchParams);
+	const currentUser = await getCurrentUser();
+
+	if (listings.length === 0) {
+		return (
+			<ClientOnly>
+				<EmptyState showReset />
+			</ClientOnly>
+		);
+	}
+
+	return (
+		<ClientOnly>
+			<Container>
+				<div
+					className="
+            pt-24
+            grid 
+            grid-cols-1 
+            sm:grid-cols-2 
+            md:grid-cols-3 
+            lg:grid-cols-4
+            xl:grid-cols-5
+            2xl:grid-cols-6
+            gap-8">
+					{listings.map((listing: any) => (
+						<ListingCard
+							currentUser={currentUser}
+							key={listing.id}
+							data={listing}
+						/>
+					))}
+				</div>
+			</Container>
+		</ClientOnly>
+	);
 }
