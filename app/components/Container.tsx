cf33d8b81f9b891d10577092bd88705f35a436e0
Muhammad Zain <ZainSoftwear11@gmail.com>
2024-03-31 15:17:18 +0500

    completed

---
+++ app/components/Container.tsx
@@ -0,0 +1,15 @@
+export default function Container({ children }: { children: React.ReactNode }) {
+	return (
+		<div
+			className="
+        max-w-[2520px]
+        mx-auto
+        xl:px-10 
+        md:px-10
+        sm:px-2
+        px-4
+      ">
+			{children}
+		</div>
+	);
+}
