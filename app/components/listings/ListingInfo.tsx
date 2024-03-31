711cace626be7ed5783a90ac6ef6cd5fa1cfa1e0
Muhammad Zain <ZainSoftwear11@gmail.com>
2024-03-31 15:18:20 +0500

    completed

---
+++ app/components/listings/ListingInfo.tsx
@@ -0,0 +1,73 @@
+"use client";
+import dynamic from "next/dynamic";
+import {
+	TListingInfoProps,
+	useCountries,
+	Avatar,
+	ListingCategory,
+} from "@/export";
+
+const Map = dynamic(() => import("../Map"), {
+	ssr: false,
+});
+
+export default function ListingInfo({
+	user,
+	description,
+	guestCount,
+	roomCount,
+	bathroomCount,
+	category,
+	locationValue,
+}: TListingInfoProps) {
+	const { getByValue } = useCountries();
+	const coordinates = getByValue(locationValue)?.latlng;
+
+	return (
+		<div className="col-span-4 flex flex-col gap-8">
+			<div className="flex flex-col gap-2">
+				<div
+					className="
+            text-xl 
+            font-semibold 
+            flex 
+            flex-row 
+            items-center
+            gap-2
+          ">
+					<div>Hosted by {user?.name}</div>
+					<Avatar src={user?.image} />
+				</div>
+				<div
+					className="
+            flex 
+            flex-row 
+            items-center 
+            gap-4 
+            font-light
+            text-neutral-500
+          ">
+					<div>{guestCount} guests</div>
+					<div>{roomCount} rooms</div>
+					<div>{bathroomCount} bathrooms</div>
+				</div>
+			</div>
+			<hr />
+			{category && (
+				<ListingCategory
+					src={category.src}
+					label={category?.label}
+					description={category?.description}
+				/>
+			)}
+			<hr />
+			<div
+				className="
+      text-lg font-light text-neutral-500">
+				{description}
+			</div>
+			<hr />
+			<Map center={coordinates} />
+		</div>
+	);
+}
