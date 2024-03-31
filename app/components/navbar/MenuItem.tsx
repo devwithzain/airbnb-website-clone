f185b6b35ec24b4e09c4f574f546c8cc0e3fc735
Muhammad Zain <ZainSoftwear11@gmail.com>
2024-03-31 15:18:45 +0500

    completed

---
+++ app/components/navbar/MenuItem.tsx
@@ -0,0 +1,18 @@
+"use client";
+import { TMenuItemProps } from "@/export";
+
+export default function MenuItem({ onClick, label }: TMenuItemProps) {
+	return (
+		<div
+			onClick={onClick}
+			className="
+        px-4 
+        py-3 
+        hover:bg-neutral-100 
+        transition
+        font-semibold
+      ">
+			{label}
+		</div>
+	);
+}
---
+++ app/components/navbar/Navbar.tsx
@@ -0,0 +1,37 @@
+import {
+	Categories,
+	Container,
+	Logo,
+	Search,
+	UserMenu,
+	TNavbarProps,
+} from "@/export";
+
+export default function Navbar({ currentUser }: TNavbarProps) {
+	return (
+		<div className="fixed w-full bg-white z-10 shadow-sm">
+			<div
+				className="
+          py-4 
+          border-b-[1px]
+        ">
+				<Container>
+					<div
+						className="
+            flex 
+            flex-row 
+            items-center 
+            justify-between
+            gap-3
+            md:gap-0
+          ">
+						<Logo />
+						<Search />
+						<UserMenu currentUser={currentUser} />
+					</div>
+				</Container>
+			</div>
+			<Categories />
+		</div>
+	);
+}
