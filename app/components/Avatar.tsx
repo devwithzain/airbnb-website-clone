bc66afe2520218e44ac51fe412c41f1e4e7469de
Muhammad Zain <ZainSoftwear11@gmail.com>
2024-03-31 12:38:26 +0500

    completed

---
+++ app/components/Avatar.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import { TAvatarProps } from "@/export";
+
+export default function Avatar({ src }: TAvatarProps) {
+	return (
+		<Image
+			className="rounded-full"
+			height="30"
+			width="30"
+			alt="Avatar"
+			src={src || "/placeholder.svg"}
+		/>
+	);
+}
