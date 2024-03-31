88d9b9632c55af8cacfe61b664cf68509a0cc02a
Muhammad Zain <ZainSoftwear11@gmail.com>
2024-03-31 15:20:27 +0500

    exported different components constants

---
+++ constants/index.ts
@@ -0,0 +1,473 @@
+import { arctic, barns, beach, camping, casesparticulares, castle, caves, chefskicthens, containers, countryside, dammusi, desert, design, domes, earthhomes, farms, golfing, hanoks, historical, houseboats, iconicities, island, lake, lakefront, luxe, mansion, misus, nationalpark, neww, offgrid, omg, pianose, play, pools, riads, rooms, ryokans, shepherdshuts, skiin, skiing, surfing, tinyhouse, topoftheworld, towers, treehouse, tropical, truli, vineyards, windmils, yurts, beachfront, amazingview, bed, boats, cabins, campus, adapted, creativespaces, aframes, trending, } from "@/public";
+
+export const termsItems = [
+   {
+      id: 1,
+      links: [
+         {
+            id: 1,
+            title: "Terms",
+            href: "/"
+         },
+         {
+            id: 2,
+            title: "Sitemap",
+            href: "/"
+         },
+         {
+            id: 3,
+            title: "Privacy",
+            href: "/"
+         },
+         {
+            id: 4,
+            title: "Your Privacy Choices",
+            href: "/"
+         },
+      ]
+   },
+   {
+      id: 2,
+      links: [
+         {
+            id: 1,
+            title: "English(US)"
+         },
+         {
+            id: 2,
+            title: "$USD"
+         },
+         {
+            id: 3,
+            title: "Facebook"
+         },
+         {
+            id: 4,
+            title: "Twitter"
+         },
+         {
+            id: 5,
+            title: "Instagram"
+         },
+      ]
+   }
+];
+
+export const footerLinks = [
+   {
+      id: 1,
+      title: "Support",
+      links: [
+         {
+            id: 1,
+            link: '/',
+            title: "Help Center"
+         },
+         {
+            id: 2,
+            link: '/',
+            title: "AirCover"
+         },
+         {
+            id: 3,
+            link: '/',
+            title: "Anti-discrimination"
+         },
+         {
+            id: 4,
+            link: '/',
+            title: "Disability support"
+         },
+         {
+            id: 5,
+            link: '/',
+            title: "Cancellation options"
+         },
+         {
+            id: 6,
+            link: '/',
+            title: "Report neighborhood concern"
+         },
+      ]
+   },
+   {
+      id: 2,
+      title: "Hosting",
+      links: [
+         {
+            id: 1,
+            link: '/',
+            title: "Airbnb your home"
+         },
+         {
+            id: 2,
+            link: '/',
+            title: "AirCover for Hosts"
+         },
+         {
+            id: 3,
+            link: '/',
+            title: "Hosting resources"
+         },
+         {
+            id: 4,
+            link: '/',
+            title: "Community forum"
+         },
+         {
+            id: 5,
+            link: '/',
+            title: "Hosting responsibly"
+         },
+         {
+            id: 6,
+            link: '/',
+            title: "Airbnb-friendly apartments"
+         },
+         {
+            id: 7,
+            link: '/',
+            title: "Join a free Hosting class"
+         },
+      ]
+   },
+   {
+      id: 3,
+      title: "Airbnb",
+      links: [
+         {
+            id: 1,
+            link: '/',
+            title: "Newsroom"
+         },
+         {
+            id: 2,
+            link: '/',
+            title: "New features"
+         },
+         {
+            id: 3,
+            link: '/',
+            title: "Careers"
+         },
+         {
+            id: 4,
+            link: '/',
+            title: "Investors"
+         },
+         {
+            id: 5,
+            link: '/',
+            title: "Gift cards"
+         },
+         {
+            id: 5,
+            link: '/',
+            title: "Airbnb.org emergency stays"
+         },
+      ]
+   },
+];
+
+export const categories = [
+   {
+      label: "Arctic",
+      src: arctic,
+      description: "This property is close to the snow!",
+   },
+   {
+      label: "Beach",
+      src: beach,
+      description: "This property is close to the beach!",
+   },
+   {
+      label: "Windmills",
+      src: windmils,
+      description: "This property is has windmills!",
+   },
+   {
+      label: "Design",
+      src: design,
+      description: "This property is modern!",
+   },
+   {
+      label: "Countryside",
+      src: countryside,
+      description: "This property is in the countryside!",
+   },
+   {
+      label: "Pools",
+      src: pools,
+      description: "This is property has a beautiful pool!",
+   },
+   {
+      label: "Islands",
+      src: island,
+      description: "This property is on an island!",
+   },
+   {
+      label: "Lake",
+      src: lake,
+      description: "This property is near a lake!",
+   },
+   {
+      label: "Skiing",
+      src: skiing,
+      description: "This property has skiing activies!",
+   },
+   {
+      label: "Castles",
+      src: castle,
+      description: "This property is an ancient castle!",
+   },
+   {
+      label: "Caves",
+      src: caves,
+      description: "This property is in a spooky cave!",
+   },
+   {
+      label: "Camping",
+      src: camping,
+      description: "This property offers camping activities!",
+   },
+   {
+      label: "Desert",
+      src: desert,
+      description: "This property is in the desert!",
+   },
+   {
+      label: "Barns",
+      src: barns,
+      description: "This property is in a barn!",
+   },
+   {
+      label: "Luxe",
+      src: luxe,
+      description: "This property is brand new and luxurious!",
+   },
+   {
+      label: "Containers",
+      src: containers,
+      description: "This property is close to the containers!",
+   },
+   {
+      label: "Lakefront",
+      src: lakefront,
+      description: "This property is close to the lakefront!",
+   },
+   {
+      label: "Domes",
+      src: domes,
+      description: "This property is has domes!",
+   },
+   {
+      label: "Dammusi",
+      src: dammusi,
+      description: "This property is dammusi!",
+   },
+   {
+      label: "Farms",
+      src: farms,
+      description: "This property is in the farms!",
+   },
+   {
+      label: "Golfing",
+      src: golfing,
+      description: "This is property has a beautiful golfing!",
+   },
+   {
+      label: "Earth Homes",
+      src: earthhomes,
+      description: "This property is on an earth homes!",
+   },
+   {
+      label: "Honoks",
+      src: hanoks,
+      description: "This property is near a honoks!",
+   },
+   {
+      label: "Historical Homes",
+      src: historical,
+      description: "This property has skiing historical homes!",
+   },
+   {
+      label: "House Boats",
+      src: houseboats,
+      description: "This property is an ancient house boats!",
+   },
+   {
+      label: "Iconic Cities",
+      src: iconicities,
+      description: "This property is in a spooky iconicities!",
+   },
+   {
+      label: "Mansion",
+      src: mansion,
+      description: "This property offers camping mansion!",
+   },
+   {
+      label: "Misus",
+      src: misus,
+      description: "This property is in the misus!",
+   },
+   {
+      label: "National Park",
+      src: nationalpark,
+      description: "This property is in a nationalpark!",
+   },
+   {
+      label: "New",
+      src: neww,
+      description: "This property is brand new and new!",
+   },
+   {
+      label: "Offgrid",
+      src: offgrid,
+      description: "This property is close to the offgrid!",
+   },
+   {
+      label: "Omg",
+      src: omg,
+      description: "This property is close to the omg!",
+   },
+   {
+      label: "Pianose",
+      src: pianose,
+      description: "This property is has pianose!",
+   },
+   {
+      label: "Play",
+      src: play,
+      description: "This property is play!",
+   },
+   {
+      label: "Riads",
+      src: riads,
+      description: "This property is in the riads!",
+   },
+   {
+      label: "Rooms",
+      src: rooms,
+      description: "This is property has a beautiful rooms!",
+   },
+   {
+      label: "Ryokans",
+      src: ryokans,
+      description: "This property is on an earth ryokans!",
+   },
+   {
+      label: "Ski In",
+      src: skiin,
+      description: "This property is near a ski-in!",
+   },
+   {
+      label: "Surfing",
+      src: surfing,
+      description: "This property has skiing historical surfing!",
+   },
+   {
+      label: "Tiny House",
+      src: tinyhouse,
+      description: "This property is an ancient house tinyhouse!",
+   },
+   {
+      label: "Top Of The World",
+      src: topoftheworld,
+      description: "This property is in a spooky topoftheworld!",
+   },
+   {
+      label: "Towers",
+      src: towers,
+      description: "This property offers camping towers!",
+   },
+   {
+      label: "Tree House",
+      src: treehouse,
+      description: "This property is in the treehouse!",
+   },
+   {
+      label: "Truli",
+      src: truli,
+      description: "This property is in a truli!",
+   },
+   {
+      label: "Tropical",
+      src: tropical,
+      description: "This property is brand new and tropical!",
+   },
+   {
+      label: "Vineyards",
+      src: vineyards,
+      description: "This property is brand new and vineyards!",
+   },
+   {
+      label: "Yurts",
+      src: yurts,
+      description: "This property is brand new and yurts!",
+   },
+   {
+      label: "Shepherd's Huts",
+      src: shepherdshuts,
+      description: "This property is brand new and shepherdshuts!",
+   },
+   {
+      label: "Chef's Kitchens",
+      src: chefskicthens,
+      description: "This property is brand new and chefskitchens!",
+   },
+   {
+      label: "Cases Particulares",
+      src: casesparticulares,
+      description: "This property is in the riads!",
+   },
+   {
+      label: "Beach Front",
+      src: beachfront,
+      description: "This is property has a beautiful beachfront!",
+   },
+   {
+      label: "Amazing View",
+      src: amazingview,
+      description: "This property is on an earth amazingview!",
+   },
+   {
+      label: "Bed",
+      src: bed,
+      description: "This property is near a bed!",
+   },
+   {
+      label: "Boats",
+      src: boats,
+      description: "This property has skiing historical boats!",
+   },
+   {
+      label: "Cabins",
+      src: cabins,
+      description: "This property is an ancient house cabins!",
+   },
+   {
+      label: "Campus",
+      src: campus,
+      description: "This property is in a spooky campus!",
+   },
+   {
+      label: "Adapted",
+      src: adapted,
+      description: "This property offers camping adapted!",
+   },
+   {
+      label: "Creative Spaces",
+      src: creativespaces,
+      description: "This property is in the creativespaces!",
+   },
+   {
+      label: "Aframes",
+      src: aframes,
+      description: "This property is in a aframes!",
+   },
+   {
+      label: "Trending",
+      src: trending,
+      description: "This property is brand new and trending!",
+   },
+];
